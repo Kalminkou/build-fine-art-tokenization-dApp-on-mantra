e11bc8d78dc636b47e38132dba9498f8dd6d7b42
aveek-goyal <156786367+aveek-goyal@users.noreply.github.com>
2024-09-27 11:33:47 +0530

    Update execute.rs

--- src/execute.rs
+++ src/execute.rs
@@ -10,7 +10,7 @@ use crate::error::ContractError;
 use crate::msg::{ExecuteMsg, InstantiateMsg, MintMsg};
 use crate::state::{Approval, Cw721Contract, TokenInfo};
 
-// version info for migration info
+
 const CONTRACT_NAME: &str = "crates.io:cw721-base";
 const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
 
@@ -26,19 +26,12 @@ where
         _info: MessageInfo,
         msg: InstantiateMsg,
     ) -> StdResult<Response<C>> {
-        set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
-
-        let info = ContractInfoResponse {
-            name: msg.name,
-            symbol: msg.symbol,
-        };
-        self.contract_info.save(deps.storage, &info)?;
-        let minter = deps.api.addr_validate(&msg.minter)?;
-        self.minter.save(deps.storage, &minter)?;
-        self.minting_allowed.save(deps.storage, &true)?;
-        self.max_mints.save(deps.storage, &msg.max_mints)?;
-        self.mint_price.save(deps.storage, &msg.mint_price)?;
-        self.token_uri.save(deps.storage, &msg.token_uri)?;
+        // Existing CW721 initialization code...
+    
+        // TODO: Initialize minting parameters (allowed, max mints, price)
+        // TODO: Save token URI for the art collection
+        // Any other art-specific initializations?
+    
         Ok(Response::default())
     }
 
@@ -50,7 +43,8 @@ where
         msg: ExecuteMsg<T>,
     ) -> Result<Response<C>, ContractError> {
         match msg {
-            ExecuteMsg::Mint(msg) => self.mint(deps, env, info, msg),
+            // TODO: Add cases for different message types
+
             ExecuteMsg::Approve {
                 spender,
                 token_id,
@@ -73,53 +67,9 @@ where
                 msg,
             } => self.send_nft(deps, env, info, contract, token_id, msg),
             ExecuteMsg::Burn { token_id } => self.burn(deps, env, info, token_id),
-            ExecuteMsg::SetMintConfig { price, max_mints } => {
-                self.set_mint_config(deps, info, price, max_mints)
-            }
-            ExecuteMsg::ToggleMinting {} => self.toggle_minting(deps, info),
         }
     }
 
-    fn set_mint_config(
-        &self,
-        deps: DepsMut,
-        info: MessageInfo,
-        price: Coin,
-        max_mints: u64,
-    ) -> Result<Response<C>, ContractError> {
-        // Only the minter (admin) can set the mint configuration
-        let minter = self.minter.load(deps.storage)?;
-        if info.sender != minter {
-            return Err(ContractError::Unauthorized {});
-        }
-        self.mint_price.save(deps.storage, &price)?;
-        self.max_mints.save(deps.storage, &max_mints)?;
-
-        Ok(Response::new()
-            .add_attribute("action", "set_mint_config")
-            .add_attribute("price", price.to_string())
-            .add_attribute("max_mints", max_mints.to_string()))
-    }
-
-    fn toggle_minting(
-        &self,
-        deps: DepsMut,
-        info: MessageInfo,
-    ) -> Result<Response<C>, ContractError> {
-        let minter = self.minter.load(deps.storage)?;
-
-        if info.sender != minter {
-            return Err(ContractError::Unauthorized {});
-        }
-
-        let mut minting_allowed = self.minting_allowed.load(deps.storage)?;
-        minting_allowed = !minting_allowed;
-        self.minting_allowed.save(deps.storage, &minting_allowed)?;
-
-        Ok(Response::new()
-            .add_attribute("action", "toggle_minting")
-            .add_attribute("minting_allowed", minting_allowed.to_string()))
-    }
 }
 
 
@@ -135,48 +85,15 @@ where
         info: MessageInfo,
         msg: MintMsg<T>,
     ) -> Result<Response<C>, ContractError> {
-        // Check if minting is allowed
-        let minting_allowed = self.minting_allowed.load(deps.storage)?;
-        if !minting_allowed {
-            return Err(ContractError::MintingDisabled {});
-        }
-
-        // Check if there are mints left
-        let token_count = self.token_count(deps.storage)?;
-        let max_mints = self.max_mints.load(deps.storage)?;
-        if token_count >= max_mints {
-            return Err(ContractError::MaxMintsReached {});
-        }
-
-        // Check payment
-        let price = self.mint_price.load(deps.storage)?;
-        if info.funds.len() != 1 || info.funds[0] != price {
-            return Err(ContractError::IncorrectPayment {});
-        }
-
-        // Retrieve the token_uri from storage
-        let token_uri = self.token_uri.load(deps.storage)?;
-        
-        // create the token
-        let token = TokenInfo {
-            owner: deps.api.addr_validate(&msg.owner)?,
-            approvals: vec![],
-            token_uri, 
-            extension: msg.extension,
-        };
-        self.tokens
-            .update(deps.storage, &token_count.to_string(), |old| match old {
-                Some(_) => Err(ContractError::Claimed {}),
-                None => Ok(token),
-            })?;
-
-        self.update_token_count(deps.storage, true)?;
-
-        Ok(Response::new()
-            .add_attribute("action", "mint")
-            .add_attribute("minter", info.sender)
-            .add_attribute("token_id", token_count.to_string()))
+        // TODO: Check if minting is allowed
+        // TODO: Check if we've reached max mints
+        // TODO: Verify correct payment
+        // TODO: Create and store the new token
+        // TODO: Update total token count
+        // TODO: Return success response
     }
+
+    //Add set_mint_config and toggle_minting
 }
 
 impl<'a, T, C> Cw721Execute<T, C> for Cw721Contract<'a, T, C>
